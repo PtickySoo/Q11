94b11ef010bf2cd098b079f009febc84b254e873
Christopher Gorman <78158751+PtickySoo@users.noreply.github.com>
2021-02-22 10:11:10 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Q11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int attempts = 0;
+            string myPassword;
+
+            attempts++;
+            Console.WriteLine("Attempt {0} - Enter Password : ", attempts);
+            myPassword = Console.ReadLine();
+
+            while(myPassword !="What"&& attempts <5)
+            {
+                //If wrong password is entered
+                Console.WriteLine("Sorry-Wrong Password");
+
+                attempts++;
+                Console.WriteLine("Attempt {0} - Enter Password :", attempts);
+                myPassword = Console.ReadLine();
+
+            }
+
+            //Password is correct to get to this stage
+            if (myPassword == "What")
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("All attempts used -- call the Authorities");
+            }
+            
+        }
+    }
+}
